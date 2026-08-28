cc67dc7ea7960fb2a6a4cf3235791f49cda910c8
rameezwahid <wahidrameez92@gmail.com>
2026-08-28 19:35:16 +0530

    completed Table Insert and some helper functions

--- dblayer/tbl.c
+++ dblayer/tbl.c
@@ -10,6 +10,7 @@
 #define SLOT_COUNT_OFFSET 2
 #define MAX_FNAME_LENGTH 80 /* used the same size for NAX file size as in test1.c */
 #define SLOT_ENTRY_SIZE 4   /* 2 bytes offset + 2 bytes length */
+#define PAGE_HEADER_SIZE 4
 
 #define checkerr(err)           \
     {                           \
@@ -28,6 +29,10 @@ int getLen(int slot, byte *pageBuf)
 {
     return (int)DecodeShort(pageBuf + slotDirAddr(slot) + 2);
 }
+void setLen(int slot, byte *pageBuf, int len)
+{
+    EncodeShort((short)len, pageBuf + slotDirAddr(slot) + 2);
+}
 
 int getNumSlots(byte *pageBuf)
 {
@@ -37,9 +42,23 @@ void setNumSlots(byte *pageBuf, int nslots)
 {
     EncodeShort((short)nslots, pageBuf + SLOT_COUNT_OFFSET);
 }
+
 int getNthSlotOffset(int slot, char *pageBuf)
 {
     return (int)DecodeShort((byte *)pageBuf + slotDirAddr(slot));
+}
+void setNtheSlotOffset(int slot, byte *pageBuf, int offset)
+{
+    EncodeShort((short)offset, pageBuf + slotDirAddr(slot));
+}
+
+int getFreeSpaceOffset(byte *pageBuf)
+{
+    return (int)DecodeShort(pageBuf + 0);
+}
+void setFreeSpaceOffset(byte *pageBuf, int offset)
+{
+    EncodeShort((short)offset, pageBuf + 0);
 }
 
 /**
@@ -104,6 +123,54 @@ int Table_Insert(Table *tbl, byte *record, int len, RecId *rid)
     // Get the next free slot on page, and copy record in the free
     // space
     // Update slot and free space index information on top of page.
+    int pageNum, err;
+    byte *pageBuf;
+    bool found = false;
+
+    err = PF_GetFirstPage(tbl->fd, &pageNum, (char **)&pageBuf);
+    while (err == PFE_OK)
+    {
+        int numSlots = getNumSlots(pageBuf);
+        int freeSpaceOffset = getFreeSpaceOffset(pageBuf);
+        int slotDirStart = PF_PAGE_SIZE - numSlots * SLOT_ENTRY_SIZE;
+        int available = slotDirStart - freeSpaceOffset - SLOT_ENTRY_SIZE;
+
+        if (numSlots < 256 && available >= len)
+        { /** 8 bit slot number has at most 256 records per page */
+            found = true;
+            break;
+        }
+
+        PF_UnfixPage(tbl->fd, pageNum, FALSE);
+        err = PF_GetNextPage(tbl->fd, &pageNum, (char **)&pageBuf);
+    }
+
+    if (!found)
+    {
+        if (err != PFE_EOF)
+        {
+            return err; // a real error instead of "no page had space"
+        }
+        err = PF_AllocPage(tbl->fd, &pageNum, (char **)&pageBuf);
+        if (err != PFE_OK)
+        {
+            return err;
+        }
+        setFreeSpaceOffset(pageBuf, PAGE_HEADER_SIZE);
+        setNumSlots(pageBuf, 0);
+    }
+
+    int numSLots = getNumSlots(pageBuf);
+    int freeSpaceOffset = getFreeSpaceOffset(pageBuf);
+
+    memcpy(pageBuf + freeSpaceOffset, record, len);
+    setNtheSlotOffset(numSLots, pageBuf, freeSpaceOffset);
+    setLen(numSLots, pageBuf, len);
+    setNumSlots(pageBuf, numSLots + 1);
+    setFreeSpaceOffset(pageBuf, freeSpaceOffset + len);
+
+    *rid = (pageNum << 9) | numSLots;
+    return PF_UnfixPage(tbl->fd, pageNum, TRUE);
 }
 
 #define checkerr(err)           \
